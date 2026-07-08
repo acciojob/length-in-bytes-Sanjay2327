0fa11691c820fd930ba6669b9dee4d2011a57de3
Sanjay Kushwaha <sanjaykushwaha80676@gmail.com>
2026-07-08 11:39:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const byteSize = (str) => {
   // write your code here
-};
-
+	const blob =new Blob([str]);
+	const size = blob.size;
+	return size;
+}; 
 // Do not change the code below
 const str = prompt("Enter some string.");
 alert(byteSize(str));
